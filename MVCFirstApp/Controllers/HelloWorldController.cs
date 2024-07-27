bb7348c53857dec01b8778970d0b02dbd8efb6d1
Axole Maranjana <axolemaranjan4@gmail.com>
2024-07-27 02:43:20 +0200

    Create first contriller

---
+++ MVCFirstApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCFirstApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is a default action...";
+        }
+
+        public string Welcome(string name, int number)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, the number is {number}");
+        }
+    }
+}
