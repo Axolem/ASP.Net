4737b749d26bff467444b62895179515b1587782
Axole Maranjana <axolemaranjan4@gmail.com>
2024-07-28 01:25:51 +0200

    Handling HTTP Requests and Responses in Web APIs

--- API/Controllers/SampleControler.cs
+++ API/Controllers/SampleControler.cs
@@ -11,5 +11,28 @@ namespace API.Controllers
         {
             return "Hello, ASP.NET Core Web API";
         }
+
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+
+            value = $"RecievedL {value.Trim()}";
+            //return value;
+            //return Ok(value);
+            //return BadRequest();
+            return NotFound(value);
+        }
+
+        //[HttpPost]
+        //public ActionResult<User> AddUser([FromBody] User user)
+        //{
+        //    return CreatedAtAction(nameof(GetUser), new object { id = user.id }, user);
+        //}
     }
+
+    //public class User
+    //{
+    //    public string id = Guid.NewGuid().ToString();
+    //}
 }
