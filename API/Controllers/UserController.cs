a57dcb7357c7a257172fa43f4b1c901a8d668458
Axole Maranjana <axolemaranjan4@gmail.com>
2024-07-28 02:09:16 +0200

    Implementing Data Validation in Web APIs . Duration: 5 minutes5 min

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+
+        public object? GetUser { get; private set; }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] User user)
+        {
+            Console.WriteLine(user.Id);
+            return ModelState.IsValid ? "Saved" : BadRequest(ModelState);
+        }
+
+        private IActionResult CreatedAtAction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255), MinLength(3), Required]
+        public required string UserName { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email")]
+        public required string Email { get; set; }
+    }
+}
