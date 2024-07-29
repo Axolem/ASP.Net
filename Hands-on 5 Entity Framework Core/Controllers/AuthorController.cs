ce30c26e7ff6c817b9d47761901d57db6f97ac93
Axole Maranjana <axolemaranjan4@gmail.com>
2024-07-29 16:02:48 +0200

    create a web page for adding new author to the database and also how to display them.

---
+++ Hands-on 5 Entity Framework Core/Controllers/AuthorController.cs
@@ -0,0 +1,25 @@
+﻿using Hands_on_5_Entity_Framework_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hands_on_5_Entity_Framework_Core.Controllers
+{
+    public class AuthorController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using EFCoreWebDemoContext context = new();
+            List<Author> model = await context.Autors.AsNoTracking().ToListAsync();
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create([Bind("FirstName, LastName")] Author author)
+        {
+            using EFCoreWebDemoContext context = new();
+            _ = context.Add(author);
+            _ = await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Hands-on 5 Entity Framework Core/Controllers/BookController.cs
@@ -0,0 +1,24 @@
+﻿using Hands_on_5_Entity_Framework_Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hands_on_5_Entity_Framework_Core.Controllers
+{
+    public class BookController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using EFCoreWebDemoContext context = new();
+            var model  = await context.Autors.Include(a => a.Books).AsNoTracking().ToListAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Create([Bind("Title, Description, AuthorId")] BookController book)
+        {
+            using EFCoreWebDemoContext context = new();
+            context.Add(book);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
